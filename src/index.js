0aa5f5f4b0bdc280df737ab1ed1888abc7a7d47d
lxs <798738148@qq.com>
2019-08-25 21:34:27 +0800

    babel和typescript配置

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import Editor from './Editor/index.tsx'
+
+ReactDom.render(<Editor />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,23 @@ module.exports = {
         path: path.join(__dirname, 'dist'),
         filename: 'index.js'
     },
+    resolve: {
+        extensions: ['.tsx', '.ts', 'jsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js|jsx|ts|tsx?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
     plugins: [
         new HtmlWebPackPlugin({
             template: path.join(__dirname, './public/index.html'),
