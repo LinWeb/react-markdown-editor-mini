f6f7ac33bc82c440f66b91f765da891e86a8d363
linxiaosheng <89007682@sfmail.sf-express.com>
2019-08-26 10:00:33 +0800

    测试是否可以提交

--- src/Editor/index.tsx
+++ src/Editor/index.tsx
@@ -1,8 +1,7 @@
-
-import React from 'react'
+import React from "react";
 
 export default class Editor extends React.Component {
-    render() {
-        return (<div>react-markdown-editor</div>)
-    }
+  render() {
+    return <div>editor</div>;
+  }
 }
