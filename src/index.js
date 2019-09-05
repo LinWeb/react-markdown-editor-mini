1ddd6ce76742a7d849c87b6ba3695dd3ee874823
linxiaosheng <89007682@sfmail.sf-express.com>
2019-09-05 10:11:51 +0800

    开启转换预览

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDom from "react-dom";
 import Editor from "./Editor/index.tsx";
 import config from "../script/example";
 import "../assets/css/icon.css";
+import "highlight.js/styles/atom-one-light.css";
 
 ReactDom.render(<Editor {...config} />, document.getElementById("root"));
