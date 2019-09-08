77113ba94f9db90b40dd6331547407ff17b29448
lxs <798738148@qq.com>
2019-09-08 17:19:51 +0800

    表格编辑项

--- src/component/editOperation/TableEditOperation/index.tsx
+++ src/component/editOperation/TableEditOperation/index.tsx
@@ -5,20 +5,40 @@ interface IProps {
     editHandle: (type: string) => void;
 }
 
-export default class TableEditOperation extends React.Component<IProps, {}> {
+interface IState {
+    hoverKey: number
+}
+
+export default class TableEditOperation extends React.Component<IProps, IState> {
     public constructor(props: Readonly<IProps>) {
         super(props)
+        this.state = {
+            hoverKey: -1
+        }
     }
 
+    onMouseEnter(key: number) {
+        this.setState(() => {
+            return {
+                hoverKey: key
+            }
+        })
+    }
     public render() {
         let { editHandle } = this.props;
+        let { hoverKey } = this.state;
         return <span
-            className="iconfont iconbiaoge table"
+            className="iconfont iconbiaoge"
             title="表格"
             onClick={() => {
 
             }}
         >
+            <ul className='table'>
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24].map((item, key) =>
+                    <li key={String(key)} className={'td ' + (key < hoverKey ? 'hover' : '')} onMouseEnter={() => { this.onMouseEnter(item) }}></li>
+                )}
+            </ul>
         </span>;
     }
 }
