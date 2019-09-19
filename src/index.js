71747c7e5ae8a3658130223ff8bed4af0b0f03f6
lxs <798738148@qq.com>
2019-09-19 23:27:42 +0800

    去除后缀

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import config from "../script/example";
 import "../assets/css/icon.css";
 import "../assets/css/mark.scss";
 
-import Editor from "./Editor/indesx.tsx";
+import Editor from "./Editor/index";
 // import Editor from '../lib/index'
 
 ReactDom.render(<Editor {...config} />, document.getElementById("root"));
