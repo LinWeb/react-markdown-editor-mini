a75af01f43bc25568dc205669f77f35cd7f31908
lxs <798738148@qq.com>
2019-09-19 23:26:30 +0800

    配置npm包环境

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom";
-import Editor from "./Editor/index.tsx";
 import config from "../script/example";
 import "../assets/css/icon.css";
 import "../assets/css/mark.scss";
+
+import Editor from "./Editor/indesx.tsx";
+// import Editor from '../lib/index'
 
 ReactDom.render(<Editor {...config} />, document.getElementById("root"));
