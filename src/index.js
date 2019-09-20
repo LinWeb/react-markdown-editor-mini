e4f93e3172ce1049a9c5bc7b3ae829b3cb743aee
linxiaosheng <89007682@sfmail.sf-express.com>
2019-09-20 11:35:37 +0800

    修改npm入口文件

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDom from "react-dom";
 import config from "../script/example";
 import "../assets/css/icon.css";
 import "../assets/css/mark.scss";
-
 import Editor from "./Editor/index";
-// import Editor from '../lib/index'
 
 ReactDom.render(<Editor {...config} />, document.getElementById("root"));
